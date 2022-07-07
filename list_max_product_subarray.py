bf74461b5d51a47261dcde072c66a3b5ecee25f0
Vivek Sheth <37891455+vivekgsheth@users.noreply.github.com>
2022-07-07 13:28:02 +0530

    Add files via upload
    Problem: https://leetcode.com/problems/maximum-product-subarray/
    Reference: NeetCode

---
+++ list_max_product_subarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        globalMax = max(nums)
+        currMax, currMin = 1, 1
+        
+        for num in nums:
+            if num == 0:
+                currMax, currMin = 1, 1
+                continue
+            
+            tempMax = currMax
+            currMax = max(num, currMax*num, currMin*num)
+            currMin = min(num, tempMax*num, currMin*num)
+            
+            globalMax = max(globalMax, currMax, currMin)
+        
+        return globalMax
