2448c3dc2dc17abde1a23e7565b345c9d7e97017
Vivek Sheth <37891455+vivekgsheth@users.noreply.github.com>
2022-07-10 14:07:02 +0530

    Add files via upload

--- permutations_LC_46.py
+++ permutations_LC_46.py
@@ -2,6 +2,19 @@ from typing import List
 
 class Solution:
     
+    def recurPermuteOpt(self, idx, n, arr, result):
+        
+        # base case
+        if idx == n:
+            result.append(arr.copy())
+            return
+        
+        for i in range(idx, n):
+            arr[idx], arr[i] = arr[i], arr[idx]
+            self.recurPermuteOpt(idx+1, n, arr, result)
+            # revert back to the original arr after recursive call
+            arr[idx], arr[i] = arr[i], arr[idx]
+
     def recurPermute(self, ds: list, freq: List[bool], n, arr, result):
         
         # base case
@@ -26,10 +39,22 @@ class Solution:
         self.recurPermute([], freq, len(nums), nums, result)
         return result
 
+        result2 = []
+        self.recurPermuteOpt(0, len(nums), nums, result2)
+        return result2
+
 print(Solution().permute([1,2,3]))
 
 '''
+recurPermute
 TC: n! * n  [permutations * looping]
 SC: O(n) + O(n) [ds + freq]
+aux SC: O(n) [depth]
+'''
+
+'''
+recurPermuteOpt
+TC: (n!) * n [permutations * looping]
+SC: O(1)
 aux SC: O(n) [depth]
 '''
